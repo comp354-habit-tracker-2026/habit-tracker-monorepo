566a1fd0e93034de883779af64b29bcae8c7ff6e
NicholasA2 <nadiohos@icloud.com>
2026-04-03 14:39:33 -0400

    #132 Tried importing core-pro to add a calendar to the dashboard

--- frontend/src/app/routes/app/dashboard.tsx
+++ frontend/src/app/routes/app/dashboard.tsx
@@ -1,9 +1,16 @@
 import { ContentLayout } from '@/components/layouts/content-layout';
+import { CAlert } from '@coreui/react-pro';
+import '@coreui/coreui-pro/dist/css/coreui.min.css';
+import React from 'react';
+import { CCalendar } from '@coreui/react-pro';
 
 function DashboardRoute() {
   return (
     <ContentLayout title="Dashboard">
       <p>Welcome back! Use the navigation to manage your habits.</p>
+      <div className="d-flex justify-content-center">
+        <CCalendar className="border rounded" locale="en-US" startDate="2024/02/13"/>
+      </div>
     </ContentLayout>
   );
 }
