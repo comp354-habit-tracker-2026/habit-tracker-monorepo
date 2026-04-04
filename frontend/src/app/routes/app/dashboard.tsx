8a40deeac249b071aa8a3f0d5a2d7152d41f5b55
rv <rvdeguzman@protonmail.com>
2026-04-03 20:42:14 -0400

    fix: calendar in dashboard

--- frontend/src/app/routes/app/dashboard.tsx
+++ frontend/src/app/routes/app/dashboard.tsx
@@ -1,7 +1,5 @@
 import { ContentLayout } from '@/components/layouts/content-layout';
-import { CAlert } from '@coreui/react-pro';
 import '@coreui/coreui-pro/dist/css/coreui.min.css';
-import React from 'react';
 import { CCalendar } from '@coreui/react-pro';
 
 function DashboardRoute() {
@@ -9,7 +7,11 @@ function DashboardRoute() {
     <ContentLayout title="Dashboard">
       <p>Welcome back! Use the navigation to manage your habits.</p>
       <div className="d-flex justify-content-center">
-        <CCalendar className="border rounded" locale="en-US" startDate="2024/02/13"/>
+        <CCalendar
+          className="border rounded"
+          locale="en-US"
+          startDate="2024/02/13"
+        />
       </div>
     </ContentLayout>
   );
