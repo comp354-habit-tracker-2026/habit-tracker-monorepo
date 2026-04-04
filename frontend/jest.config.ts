9fc62ae2b39962161dcec85c040005fdda712ec0
LAPTOP-KJD6UCS0\amand <goldyblockslol@gmail.com>
2026-04-03 20:47:47 -0400

    #169 Setting up Jest and Cypress

---
+++ frontend/jest.config.ts
@@ -0,0 +1,35 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+
+  roots: ["<rootDir>/jest"],
+  testMatch: ["**/*.test.ts", "**/*.test.tsx"],
+  
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", {
+      tsconfig: {
+        jsx: "react-jsx"
+      }
+    }],
+  },
+  
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  
+  setupFilesAfterEnv: ["<rootDir>/jest/setupTests.ts"],
+
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/main.tsx",
+  ],
+  
+  verbose: true,
+};
+
+export default config;
