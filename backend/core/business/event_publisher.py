6e435174cc669eafa20b618e059fa798bd48fdab
Shiyuan Zhang <shiyuanzhang.jessica@gmail.com>
2026-04-05 18:00:56 -0400

    Add new scripts and update deduplication
    Update deduplication.py and pipelin.py. Add new scripts event_publisher.py and hooks.py

---
+++ backend/core/business/event_publisher.py
@@ -0,0 +1,23 @@
+'''
+Name: Shiyuan Zhang (40228185)
+File: event_publisher.py
+Issue: #149
+
+Goal: 
+Publish an event to the Eventhub when Activity data is preprocessed and ready for downstream tasks
+Log success/failure
+
+adapter -> normalizer -> deduplication -> create event message -> publish
+
+'''
+class EventPublisher:
+    # Builds msg
+    def publish_event(self, activity, status):
+        event = {
+            'event_type': 'ActivityCreatedEvent',
+            'status': status,
+            'provider': activity.get('provider'),
+            'external_id': activity.get('external_id'),
+            'payload': activity
+        }
+        print('Publishing event:', event)
---
+++ backend/core/business/hooks.py
@@ -0,0 +1,24 @@
+'''
+Name: Shiyuan Zhang (40228185)
+File: hooks.py
+Issue: #82
+
+Goal: 
+Publish an event to the Eventhub when Activity data is preprocessed and ready for downstream tasks
+Log success/failure
+
+'''
+
+class LoggingHook:
+    def log_details(self, status, changed_fields, activity):
+        print(
+            f"provider: {activity.get('provider')}, "
+            f"external_id: {activity.get('external_id')}, "
+            f'status: {status}'
+        )
+        if status == 'SKIP':
+            print('Activity already exists and has no changes. Skip.')
+        if status == 'NEW':
+            print('New Activity saved.')
+        if status == 'UPDATE' and changed_fields:
+            print(f'Existing activity found, differences in fields {changed_fields}. Activity updated.')
