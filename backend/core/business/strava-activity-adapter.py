ded0a319e3ac7344553bfd6ff42d9cdf595dca89
JeffBrin <bringolfj@hotmail.com>
2026-04-06 16:26:26 -0400

    Small change to strava activity adapter, commiting to change branches

--- backend/core/business/strava-activity-adapter.py
+++ backend/core/business/strava-activity-adapter.py
@@ -6,21 +6,18 @@ class StravaActivityAdapter(ActivityAdapter):
     def __init__(self):
         super().__init__(ActivitySource.STRAVA)
 
-    @abstractmethod
     def parse(self, raw_input_data):
         """Parse raw activity data into a standard Activity object."""
-        pass
+        raise NotImplementedError()
 
     #validate(raw_input_data) -> bool
     #Validate raw input data before parsing.
-    @abstractmethod
     def validate(self, raw_input_data):
         """Validate raw data before parsing."""
-        pass
+        raise NotImplementedError()
 
     #mapToActivity(self, raw_input_data) -> Activity object
     #Convert raw input data into a standardized Activity object.
-    @abstractmethod
     def mapToActivity(self, raw_input_data):
         """Convert raw input data into a standardized Activity object."""
-        pass
+        raise NotImplementedError()
