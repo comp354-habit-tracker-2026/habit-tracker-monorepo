d474ae900749d631e89c178346af7bec550c9723
JeffBrin <bringolfj@hotmail.com>
2026-04-06 16:37:36 -0400

    Added Kabeya's changes since their branch was made off of main.

---
+++ backend/activities/data/mywhoosh_adapter.py
@@ -0,0 +1,28 @@
+"""Name: Kabeya Ngoyi
+    Student ID: 27214545
+    File: mywhoosh_adapter.py
+    
+    Description: 
+    This file contains the MyWhoosh adapter. It converts raw MyWhoosh
+    activity into the unified Activity format used by the system."""
+
+class MyWhooshAdapter:
+    def __init__(self):
+        self.provider = "mywhoosh"
+
+    def transform(self, raw_activity):
+        """
+        Converting raw MyWhoosh data into Activity format
+        """
+
+        return {
+            "activity_type": raw_activity.get("type", "unknwon"),
+            "duration": raw_activity.get("duration", 0), 
+            "date": raw_activity.get("date"), 
+            "provider": self.provider, 
+            "external_id": raw_activity.get("id"), 
+            "raw_data": raw_activity, 
+            "distance": raw_activity.get("distance"), 
+            "calories": raw_activity.get("calories")
+        }
+    
