c255bba6d6b2ce5b769203e4cbe4bf5ebe13d6bf
azmainj26 <azmainjashim3@gmail.com>
2026-04-07 12:12:48 -0400

    Add PasswordReset views to exports
    Signed-off-by: azmainj26 <azmainjashim3@gmail.com>

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,11 @@
-from users.presentation.views import RegisterView
+from users.presentation.views import (
+    RegisterView,
+    PasswordResetRequestView,
+    PasswordResetConfirmView,
+)
 
-__all__ = ["RegisterView"]
+__all__ = [
+    "RegisterView",
+    "PasswordResetRequestView",
+    "PasswordResetConfirmView",
+]
