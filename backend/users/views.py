3427413b0b09f2457449a4d2d041f4d71f849b9c
azmainj26 <azmainjashim3@gmail.com>
2026-04-07 10:56:34 -0400

    Add PasswordReset views to users module
    Signed-off-by: azmainj26 <azmainjashim3@gmail.com>

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,19 @@
 from users.presentation.views import RegisterView
 
 __all__ = ["RegisterView"]
+
+
+
+
+
+from users.presentation.views import (
+    RegisterView,
+    PasswordResetRequestView,
+    PasswordResetConfirmView
+)
+
+__all__ = [
+    "RegisterView",
+    "PasswordResetRequestView",
+    "PasswordResetConfirmView"
+]
