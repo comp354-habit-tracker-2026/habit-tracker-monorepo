cf0405a82687959b7a375296b7f83434cc393b67
samishamalhotra <117317534+samishamalhotra@users.noreply.github.com>
2026-04-07 18:40:13 -0400

    feat: add mark as read URL route - issue #165
    Signed-off-by: samishamalhotra <117317534+samishamalhotra@users.noreply.github.com>

--- backend/notifications/urls.py
+++ backend/notifications/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from .views import NotificationsHealthView
 from .views import ViewNotifications
+from .views import MarkNotificationAsRead
 
 
 urlpatterns = [
     path("health/", NotificationsHealthView.as_view(), name="notifications_health"),
     path('notifications/<int:user_id>/', ViewNotifications.as_view()),
+    path('notifications/<int:notification_id>/read/', MarkNotificationAsRead.as_view(), name="mark_notification_as_read"),
 ]
