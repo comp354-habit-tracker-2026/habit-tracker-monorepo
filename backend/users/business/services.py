07e53743e7c126549eecd7e4ed3c2250fe533913
camj92hp <camj95hp@gmail.com>
2026-04-13 18:45:36 -0400

    added some methods for locking account

--- backend/users/business/services.py
+++ backend/users/business/services.py
@@ -1,11 +1,65 @@
+from django.utils import timezone
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework import status
+from rest_framework.exceptions import APIException
+
 from core.business import BaseService
 from users.data import UserRepository
 
 
-class UserRegistrationService(BaseService):
+MAX_FAILED_ATTEMPTS = 5
+LOCKOUT_MINUTES = 30
+
+class UserService(BaseService):
     def __init__(self, repository=None):
         self.repository = repository or UserRepository()
 
     def register_user(self, validated_data):
         validated_data.pop("password2", None)
         return self.repository.create_user(**validated_data)
+
+
+    def check_lockout(self, username):
+        """
+        Checks if account is locked.
+        Raises 423 if locked, does nothing if not locked.
+        """
+        user = self.repository.get_by_username(username)
+
+        if user is None:
+            return  # user doesn't exist, let SimpleJWT handle the 401
+
+        if user.locked_until and user.locked_until > timezone.now():
+            raise AccountLockedException()
+
+    def record_failed_attempt(self, username):
+        """
+        Increments failed attempts and locks account if threshold reached.
+        """
+        user = self.repository.get_by_username(username)
+
+        if user is None:
+            return  # user doesn't exist, nothing to track
+
+        self.repository.increment_failed_attempts(user)
+
+        if user.failed_login_attempts >= MAX_FAILED_ATTEMPTS:
+            self.repository.increment_failed_attempts(user)
+
+    def record_successful_login(self, username):
+        """
+        Resets failed attempts after successful login.
+        """
+        user = self.repository.get_by_username(username)
+
+        if user is None:
+            return
+
+        self.repository.reset_failed_attempts(user)
+
+
+class AccountLockedException(APIException):
+    status_code = status.HTTP_423_LOCKED
+    default_detail = "Account temporarily locked due to too many failed attempts. Try again in 30 minutes."
+    default_code = "account_locked"
+      
