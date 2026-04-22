7a0462998410df89b5777d48090e3caebed330d7
Derek J. Morin <morinjderek@gmail.com>
2026-04-22 16:21:56 -0400

    Test for merge conflicts

--- backend/analytics/views.py
+++
@@ -1,8 +0,0 @@
-# from analytics.presentation.views import AnalyticsOverviewView
-
-# __all__ = ["AnalyticsOverviewView"]
-
-# from django.http import JsonResponse
-# from analytics.team12.services import Team12AnalyticsService
-
-# service = Team12AnalyticsService()
