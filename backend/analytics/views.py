e3bdd627a884872e2527f39597c070296752cdce
Derek J. Morin <morinjderek@gmail.com>
2026-04-22 16:19:19 -0400

    Test for merge conflicts

--- backend/analytics/views.py
+++ backend/analytics/views.py
@@ -1,24 +1,8 @@
-from analytics.presentation.views import AnalyticsOverviewView
+# from analytics.presentation.views import AnalyticsOverviewView
 
-__all__ = ["AnalyticsOverviewView"]
+# __all__ = ["AnalyticsOverviewView"]
 
-from django.http import JsonResponse
-from analytics.team12.services import Team12AnalyticsService
+# from django.http import JsonResponse
+# from analytics.team12.services import Team12AnalyticsService
 
-service = Team12AnalyticsService()
-
-# Made with the help of an LLM
-def personal_record_view(request, habit_id):
-    user = request.user
-    metric_type = request.GET.get("metricType")
-
-    # try:
-    #     result = service.personal_record_for_habit(user, habit_id, metric_type)
-
-    #     if result is None:
-    #         return JsonResponse(None, safe=False)
-
-    #     return JsonResponse(result)
-
-    # except ValueError:
-    #     return JsonResponse({"error": "Invalid metric type"}, status=400)
+# service = Team12AnalyticsService()
